d4b5897f2ef0ccfe15e37503c2da04f8466db3f1
conchboy <panchaocn@hotmail.com>
2021-01-21 16:59:20 +0800

    updated namecards management: cards_main.py

--- cards_tools.py
+++ cards_tools.py
@@ -1,4 +1,5 @@
 # 显示菜单函数
+card_dict ={}
 def show_menu():
     """显示菜单"""
     print("*" * 40)
@@ -13,12 +14,27 @@ def new_card():
     """新增名片"""
     print("-" * 40)
     print("新增名片")
+    print("请输入名片的信息：")
+    name_char = input("请输入名字：")
+    qq_char = input("请输入QQ号码：")
+    phone_char = input("请输入电话号码：")
+    email_char = input("请输入电子邮件地址：")
+    card_dict = {"name": name_char, "QQ": qq_char, "phone": phone_char, "E-mail": email_char}
+    print("谢谢输入，新增的名片是： % s" % card_dict)
 
 
 def show_all():
     """显示全部"""
     print("-" * 40)
     print("显示全部")
+    for card_title in ["姓名", "QQ号码", "电话号码", "Email"]:
+        print("%s\t\t" % card_title, end="")
+    print("")
+    # (card_dict["name"], card_dict["QQ"], card_dict["phone"], card_dict["E-mail"])
+    print("*" * 40)
+
+
+
 
 
 def search_card():
